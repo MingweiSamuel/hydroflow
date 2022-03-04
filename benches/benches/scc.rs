9c68aa38cfc03e861cbf196c38f4e3bdc9cefdce
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-03-04 09:06:13 -0800

    WIP

--- benches/benches/scc.rs
+++ benches/benches/scc.rs
@@ -1,9 +1,8 @@
-use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet};
+use std::collections::{HashMap, HashSet};
 use std::io::{BufRead, BufReader, Cursor};
 use std::time::Duration;
 
 use criterion::Criterion;
-use hydroflow::scheduled::input::Give;
 use once_cell::sync::OnceCell;
 use rand::Rng;
 
@@ -32,8 +31,8 @@ fn graph_data() -> &'static GraphData {
     })
 }
 
-fn scc_labels() -> &'static [(usize, usize)] {
-    static SCC_LABELS: OnceCell<Vec<(usize, usize)>> = OnceCell::new();
+fn scc_labels() -> &'static [usize] {
+    static SCC_LABELS: OnceCell<Vec<usize>> = OnceCell::new();
     &*SCC_LABELS.get_or_init(|| {
         let cursor = Cursor::new(include_bytes!("scc_labels.txt"));
         let reader = BufReader::new(cursor);
@@ -41,12 +40,8 @@ fn scc_labels() -> &'static [(usize, usize)] {
         reader
             .lines()
             .map(|line| {
-                let line = line.unwrap();
-                let mut nums = line.split_whitespace();
-                let a = nums.next().unwrap().parse().unwrap();
-                let b = nums.next().unwrap().parse().unwrap();
-                assert!(nums.next().is_none());
-                (a, b)
+                let label = line.unwrap().parse().unwrap();
+                label
             })
             .collect()
     })
@@ -72,8 +67,8 @@ pub fn naive_n3(
     size: usize,
     forw: &HashMap<usize, Vec<usize>>,
     back: &HashMap<usize, Vec<usize>>,
-) -> Vec<(usize, usize)> {
-    let labels: Vec<_> = (0..size)
+) -> Vec<usize> {
+    (0..size)
         .map(|v| {
             let mut visited_forw = HashSet::new();
             let mut visited_back = HashSet::new();
@@ -96,17 +91,16 @@ pub fn naive_n3(
                 &mut |_| {},
             );
 
-            (v, label)
+            label
         })
-        .collect();
-    labels
+        .collect()
 }
 
 pub fn kosaraju(
     size: usize,
     forw: &HashMap<usize, Vec<usize>>,
     back: &HashMap<usize, Vec<usize>>,
-) -> Vec<(usize, usize)> {
+) -> Vec<usize> {
     let mut seen = HashSet::new();
     let mut order = Vec::with_capacity(size);
     for v in 0..size {
@@ -129,44 +123,48 @@ pub fn kosaraju(
         .map(|v| {
             let root = roots[&v];
             let label = root_label[&root];
-            (v, label)
+            label
         })
         .collect()
 }
 
 pub fn fleischer_subroutine(
-    subgraph_vertices: Vec<usize>,
-    forw: &HashMap<usize, Vec<usize>>,
-    back: &HashMap<usize, Vec<usize>>,
-    scc_found: &mut impl FnMut(usize, Vec<usize>),
-) -> (Vec<usize>, Vec<usize>, Vec<usize>) {
+    forw: &AdjList,
+    back: &AdjList,
+) -> (
+    (usize, Vec<usize>),
+    HashSet<usize>,
+    HashSet<usize>,
+    HashSet<usize>,
+) {
+    let subgraph_vertices: Vec<_> = forw
+        .keys()
+        .filter(|&v| !back.contains_key(v))
+        .chain(back.keys())
+        .cloned()
+        .collect();
     assert!(!subgraph_vertices.is_empty());
     let pivot = subgraph_vertices[rand::thread_rng().gen_range(0..subgraph_vertices.len())];
 
-    let mut visited_forw = BTreeSet::new();
-    dfs(
-        forw,
-        pivot,
-        &mut |v| subgraph_vertices.binary_search(&v).is_ok() && visited_forw.insert(v),
-        &mut |_| {},
-    );
+    let mut visited_forw = HashSet::new();
+    dfs(forw, pivot, &mut |v| visited_forw.insert(v), &mut |_| {});
 
     let mut scc_max = 0;
     let mut scc_members = Vec::new();
 
-    let mut subgraph_back = BinaryHeap::new();
+    let mut subgraph_back = HashSet::new();
 
     let mut visited_back = HashSet::new();
     dfs(
         back,
         pivot,
         &mut |v| {
-            if subgraph_vertices.binary_search(&v).is_ok() && visited_back.insert(v) {
-                if visited_forw.contains(&v) {
+            if visited_back.insert(v) {
+                if visited_forw.take(&v).is_some() {
                     scc_max = std::cmp::max(scc_max, v);
                     scc_members.push(v);
                 } else {
-                    subgraph_back.push(v);
+                    subgraph_back.insert(v);
                 }
                 true
             } else {
@@ -175,51 +173,66 @@ pub fn fleischer_subroutine(
         },
         &mut |_| {},
     );
-    (scc_found)(scc_max, scc_members);
 
-    let mut subgraph_othr = subgraph_vertices;
-    subgraph_othr.retain(|v| !visited_forw.contains(v) && !visited_back.contains(v));
-    let subgraph_forw: Vec<_> = visited_forw
+    println!("A {}", subgraph_vertices.len());
+
+    let subgraph_othr: HashSet<usize> = subgraph_vertices
         .into_iter()
-        .filter(|v| !visited_back.contains(v))
+        .filter(|v| !visited_forw.contains(v) && !visited_back.contains(v))
         .collect();
-    let subgraph_back = subgraph_back.into_sorted_vec();
+    let subgraph_forw = visited_forw;
 
-    (subgraph_othr, subgraph_forw, subgraph_back)
+    println!(
+        "B {} {} {} {}",
+        scc_members.len(),
+        subgraph_othr.len(),
+        subgraph_forw.len(),
+        subgraph_back.len()
+    );
+
+    (
+        (scc_max, scc_members),
+        subgraph_othr,
+        subgraph_forw,
+        subgraph_back,
+    )
+}
+
+pub fn filter_subgraph(verts: &HashSet<usize>, adj_list: &AdjList) -> AdjList {
+    adj_list
+        .iter()
+        .filter(|&(k, _v)| verts.contains(k))
+        .map(|(&k, val)| {
+            (
+                k,
+                val.iter().filter(|&v| verts.contains(v)).copied().collect(),
+            )
+        })
+        .collect()
 }
 
 pub fn fleischer_single(
     size: usize,
     forw: &HashMap<usize, Vec<usize>>,
     back: &HashMap<usize, Vec<usize>>,
-) -> Vec<(usize, usize)> {
-    fn fleischer_recursive(
-        subgraph_vertices: Vec<usize>,
-        forw: &HashMap<usize, Vec<usize>>,
-        back: &HashMap<usize, Vec<usize>>,
-        output: &mut BTreeMap<usize, usize>,
-    ) {
+) -> Vec<usize> {
+    let mut output = vec![0; size];
+    let mut subgraph_stack = vec![(0..size).collect::<HashSet<_>>()];
+    while let Some(subgraph_vertices) = subgraph_stack.pop() {
         if subgraph_vertices.is_empty() {
-            return;
+            continue;
         }
-        let (subgraph_othr, subgraph_forw, subgraph_back) = fleischer_subroutine(
-            subgraph_vertices,
-            forw,
-            back,
-            &mut |scc_max, scc_members| {
-                for v in scc_members {
-                    output.insert(v, scc_max);
-                }
-            },
-        );
-        fleischer_recursive(subgraph_othr, forw, back, output);
-        fleischer_recursive(subgraph_forw, forw, back, output);
-        fleischer_recursive(subgraph_back, forw, back, output);
-    }
+        let curr_forw = filter_subgraph(&subgraph_vertices, forw);
+        let curr_back = filter_subgraph(&subgraph_vertices, back);
 
-    let mut output = Default::default();
-    fleischer_recursive((0..size).collect(), forw, back, &mut output);
-    output.into_iter().collect()
+        let ((scc_max, scc_members), subgraph_othr, subgraph_forw, subgraph_back) =
+            fleischer_subroutine(&curr_forw, &curr_back);
+        for v in scc_members {
+            output[v] = scc_max;
+        }
+        subgraph_stack.extend([subgraph_forw, subgraph_back, subgraph_othr]);
+    }
+    output
 }
 
 pub fn fleischer_multi(
@@ -227,33 +240,35 @@ pub fn fleischer_multi(
     forw: &HashMap<usize, Vec<usize>>,
     back: &HashMap<usize, Vec<usize>>,
     num_threads: usize,
-) -> Vec<(usize, usize)> {
+) -> Vec<usize> {
     use rayon::{Scope, ThreadPoolBuilder};
-    use std::sync::mpsc::{self, SyncSender};
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    // use std::sync::mpsc::{self, Sender};
 
     fn fleischer_rayon<'a>(
         scope: &Scope<'a>,
-        subgraph_vertices: Vec<usize>,
+        subgraph_vertices: HashSet<usize>,
         forw: &'a HashMap<usize, Vec<usize>>,
         back: &'a HashMap<usize, Vec<usize>>,
-        output: &'a SyncSender<(usize, Vec<usize>)>,
+        output: &'a [AtomicUsize],
     ) {
         if subgraph_vertices.is_empty() {
             return;
         }
-        let (subgraph_othr, subgraph_forw, subgraph_back) = fleischer_subroutine(
-            subgraph_vertices,
-            forw,
-            back,
-            &mut |scc_max, scc_members| {
-                output
-                    .try_send((scc_max, scc_members))
-                    .expect("Backpressure!")
-            },
-        );
-        scope.spawn(|scope| fleischer_rayon(scope, subgraph_othr, forw, back, output));
-        scope.spawn(|scope| fleischer_rayon(scope, subgraph_forw, forw, back, output));
-        scope.spawn(|scope| fleischer_rayon(scope, subgraph_back, forw, back, output));
+
+        let curr_forw = filter_subgraph(&subgraph_vertices, forw);
+        let curr_back = filter_subgraph(&subgraph_vertices, back);
+
+        let ((scc_max, scc_members), subgraph_othr, subgraph_forw, subgraph_back) =
+            fleischer_subroutine(&curr_forw, &curr_back);
+        for v in scc_members {
+            output[v].store(scc_max, Ordering::Relaxed);
+        }
+
+        let (output_a, output_b, output_c) = (output.clone(), output.clone(), output);
+        scope.spawn(|scope| fleischer_rayon(scope, subgraph_othr, forw, back, output_a));
+        scope.spawn(|scope| fleischer_rayon(scope, subgraph_forw, forw, back, output_b));
+        scope.spawn(|scope| fleischer_rayon(scope, subgraph_back, forw, back, output_c));
     }
 
     let threadpool = ThreadPoolBuilder::new()
@@ -261,15 +276,16 @@ pub fn fleischer_multi(
         .build()
         .expect("Failed to build threadpool.");
 
-    let (output_sender, output_receiver) = mpsc::sync_channel(800);
-    {
-        threadpool.scope(move |scope| {
-            let output_sender = output_sender;
-            fleischer_rayon(scope, (0..size).collect(), forw, back, &output_sender);
-        });
-    }
+    let mut output: Vec<AtomicUsize> = Vec::with_capacity(size);
+    output.resize_with(size, Default::default);
 
-    vec![]
+    threadpool.scope(|scope| {
+        let output_slice = &*output;
+        scope.spawn(move |scope| {
+            fleischer_rayon(scope, (0..size).collect(), forw, back, output_slice);
+        });
+    });
+    output.iter().map(|x| x.load(Ordering::Relaxed)).collect()
 }
 
 pub fn crit_naive_n3(c: &mut Criterion) {
@@ -313,13 +329,15 @@ pub fn crit_fleischer_single(c: &mut Criterion) {
     });
 }
 
-pub fn crit_fleischer_4(c: &mut Criterion) {
+pub fn crit_fleischer_8(c: &mut Criterion) {
     let &(size, ref forw, ref back) = graph_data();
     let expected = scc_labels();
 
-    c.bench_function("scc/fleischer_4", |b| {
+    // println!("{:?}", std::thread::available_parallelism());
+
+    c.bench_function("scc/fleischer_8", |b| {
         b.iter(|| {
-            let labels = fleischer_multi(size, forw, back, 4);
+            let labels = fleischer_multi(size, forw, back, 8);
             assert_eq!(expected, &*labels);
         });
     });
@@ -329,35 +347,45 @@ criterion::criterion_group!(
     name = scc;
     config = Criterion::default().sample_size(10).measurement_time(Duration::from_secs(10));
     targets =
-        crit_naive_n3,
-        crit_kosaraju,
+        // crit_kosaraju,
+        // crit_fleischer_8,
         crit_fleischer_single,
-        crit_fleischer_4,
+        // crit_naive_n3, // TOO SLOW
 );
 #[cfg(not(feature = "scc_graphgen"))]
 criterion::criterion_main!(scc);
 
 #[cfg(feature = "scc_graphgen")]
 fn main() {
-    use rand::{Rng, SeedableRng};
+    use rand::seq::SliceRandom;
+    use rand::SeedableRng;
     use std::io::Write;
 
-    let size = 2_000;
+    let size = 200_000;
 
     let mut rng = rand::rngs::SmallRng::seed_from_u64(892348932490);
+
+    let mut rename: Vec<_> = (0..size).collect();
+    rename.shuffle(&mut rng);
 
     let mut forw = AdjList::new();
     let mut back = AdjList::new();
     {
         let mut out =
             std::io::BufWriter::new(std::fs::File::create("benches/scc_edges.txt").unwrap());
-        for a in 0..size {
-            for b in 0..size {
+        for a in 0_usize..size {
+            for b in (a.saturating_sub(20))..(std::cmp::min(size, a + 20)) {
                 if a == b {
                     continue;
                 }
                 let d = std::cmp::max(a, b) - std::cmp::min(a, b);
-                let p = f64::max(0.02 / (d as f64), 1.0 / (1.0 + ((d as f64) - 1.0).exp()));
+                let p = f64::max(
+                    20.0 / ((size as f64) * (d as f64)),
+                    1.0 / (1.0 + ((d as f64) - 1.8).exp()),
+                );
+
+                let a = rename[a];
+                let b = rename[b];
                 if rng.gen_bool(p) {
                     writeln!(out, "{} {}", a, b).unwrap();
                     forw.entry(a).or_default().push(b);
@@ -365,16 +393,26 @@ fn main() {
                 }
             }
         }
+        out.flush().unwrap();
     }
+    println!("Done writing graph.");
     {
         let mut out =
             std::io::BufWriter::new(std::fs::File::create("benches/scc_labels.txt").unwrap());
-        let labels = naive_n3(size, &forw, &back);
-        for &(v, label) in &labels {
-            writeln!(out, "{} {}", v, label).unwrap();
+        let labels = kosaraju(size, &forw, &back);
+
+        let mut label_counts = HashMap::<_, usize>::new();
+        for label in labels {
+            writeln!(out, "{}", label).unwrap();
+            *label_counts.entry(label).or_default() += 1;
+        }
+        let num_labels = label_counts.len();
+
+        let mut distribution = BTreeMap::<_, usize>::new();
+        for (_, count) in label_counts {
+            *distribution.entry(count).or_default() += 1;
         }
 
-        let label_set: HashSet<_> = labels.iter().map(|(_v, label)| label).collect();
-        println!("Number of SCCs: {}", label_set.len());
+        println!("{}\n{:#?}", num_labels, distribution);
     }
 }
