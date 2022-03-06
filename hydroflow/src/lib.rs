1e50ead955193749c440dc473755c7ed295ecefe
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-03-06 14:13:00 -0800

    minimize ICE

--- hydroflow/src/lib.rs
+++ hydroflow/src/lib.rs
@@ -1,3 +1,20 @@
 #![feature(type_alias_impl_trait)]
 #![feature(generic_associated_types)]
-pub mod lang;
+
+use std::collections::HashSet;
+use std::hash::Hash;
+
+pub trait Collection<K, V> {
+    type Keys<'s>: Iterator<Item = &'s K>
+    where
+        K: 's,
+        Self: 's;
+    fn keys(&self) -> Self::Keys<'_>;
+}
+
+impl<K: 'static + Eq + Hash> Collection<K, ()> for HashSet<K> {
+    type Keys<'s> = std::collections::hash_set::Iter<'s, K>;
+    fn keys(&self) -> Self::Keys<'_> {
+        self.iter()
+    }
+}
