f07d58c58b61596b1d7487ff9564abeb33ac78b6
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-03-23 13:51:41 -0700

    Add `write_flow_graph` to `Subgraph` trait

--- hydroflow/src/scheduled/subgraph.rs
+++ hydroflow/src/scheduled/subgraph.rs
@@ -1,11 +1,17 @@
 use super::context::Context;
+use super::graph::FlowGraph;
 
 /**
  * Represents a compiled subgraph. Used internally by [Dataflow] to erase the input/output [Handoff] types.
  */
 pub(crate) trait Subgraph {
     // TODO: pass in some scheduling info?
+    /// Run the subgraph for one quantum of time.
+    /// For now that is as much work as is available.
     fn run(&mut self, context: Context<'_>);
+
+    /// Write the subgraph to `flow_graph`. By default does nothing.
+    fn write_flow_graph(&self, _flow_graph: &mut FlowGraph) {}
 }
 impl<F> Subgraph for F
 where
