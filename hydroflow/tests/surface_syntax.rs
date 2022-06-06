906aea13914bb1ac338b65bc4c6e2558d8db1639
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-06-06 11:03:39 -0700

    custom types WIP

--- hydroflow/tests/surface_syntax.rs
+++ hydroflow/tests/surface_syntax.rs
@@ -3,6 +3,6 @@ use hydroflow::hydroflow_parser;
 #[test]
 pub fn test_parser_basic() {
     hydroflow_parser! {
-        a() >> b() >> c()
+        a()
     }
 }
--- hydroflow_macro/src/lib.rs
+++ hydroflow_macro/src/lib.rs
@@ -4,73 +4,97 @@ use quote::{quote, ToTokens};
 use syn::parse::{Parse, ParseStream};
 use syn::punctuated::Punctuated;
 use syn::token::Paren;
-use syn::{parenthesized, parse_macro_input, Expr, ExprCall, ExprPath, Token};
+use syn::{parenthesized, parse_macro_input, Expr, ExprParen, ExprPath, Token};
 
-struct Pipeline {
-    pub elems: Punctuated<Operator, Token![>>]>,
-}
-impl Parse for Pipeline {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        let mut elems = Punctuated::new();
+// struct Pipeline {
+//     pub elems: Punctuated<Op, Token![>>]>,
+// }
+// impl Parse for Pipeline {
+//     fn parse(input: ParseStream) -> syn::Result<Self> {
+//         let mut elems = Punctuated::new();
 
-        while !input.is_empty() {
-            let first = input.parse()?;
-            elems.push_value(first);
-            if input.is_empty() {
-                break;
-            }
-            let punct = input.parse()?;
-            elems.push_punct(punct);
-        }
+//         while !input.is_empty() {
+//             let first = input.parse()?;
+//             elems.push_value(first);
+//             if input.is_empty() {
+//                 break;
+//             }
+//             let punct = input.parse()?;
+//             elems.push_punct(punct);
+//         }
 
-        Ok(Self { elems })
-    }
-}
-impl ToTokens for Pipeline {
-    fn to_tokens(&self, tokens: &mut TokenStream) {
-        self.elems.to_tokens(tokens);
-    }
-}
+//         Ok(Self { elems })
+//     }
+// }
+// impl ToTokens for Pipeline {
+//     fn to_tokens(&self, tokens: &mut TokenStream) {
+//         self.elems.to_tokens(tokens);
+//     }
+// }
 
-struct Operator {
-    pub path: ExprPath,
-    pub paren_token: Paren,
-    pub args: Punctuated<Expr, Token![,]>,
-}
-impl Parse for Operator {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        let path = input.parse()?;
+// enum Op {
+//     Fn(OpFn),
+//     Paren(ExprParen),
+// }
+// impl Parse for Op {
+//     fn parse(input: ParseStream) -> syn::Result<Self> {
+//         if input.peek(Paren) {
+//             let expr_paren = input.parse()?;
+//             Ok(Self::Paren(expr_paren))
+//         } else {
+//             let op_fn = input.parse()?;
+//             Ok(Self::Fn(op_fn))
+//         }
+//     }
+// }
+// impl ToTokens for Op {
+//     fn to_tokens(&self, tokens: &mut TokenStream) {
+//         match self {
+//             Op::Fn(op_fn) => op_fn.to_tokens(tokens),
+//             Op::Paren(expr_paren) => expr_paren.to_tokens(tokens),
+//         }
+//     }
+// }
 
-        let content;
-        let paren_token = parenthesized!(content in input);
-        let mut args = Punctuated::new();
+// struct OpFn {
+//     pub path: ExprPath,
+//     pub paren_token: Paren,
+//     pub args: Punctuated<Expr, Token![,]>,
+// }
+// impl Parse for OpFn {
+//     fn parse(input: ParseStream) -> syn::Result<Self> {
+//         let path = input.parse()?;
 
-        while !content.is_empty() {
-            let first = content.parse()?;
-            args.push_value(first);
-            if content.is_empty() {
-                break;
-            }
-            let punct = content.parse()?;
-            args.push_punct(punct);
-        }
+//         let content;
+//         let paren_token = parenthesized!(content in input);
+//         let mut args = Punctuated::new();
 
-        Ok(Self {
-            path,
-            paren_token,
-            args,
-        })
-    }
-}
-impl ToTokens for Operator {
-    fn to_tokens(&self, tokens: &mut TokenStream) {
-        todo!()
-    }
-}
+//         while !content.is_empty() {
+//             let first = content.parse()?;
+//             args.push_value(first);
+//             if content.is_empty() {
+//                 break;
+//             }
+//             let punct = content.parse()?;
+//             args.push_punct(punct);
+//         }
+
+//         Ok(Self {
+//             path,
+//             paren_token,
+//             args,
+//         })
+//     }
+// }
+// impl ToTokens for OpFn {
+//     fn to_tokens(&self, tokens: &mut TokenStream) {
+//         todo!()
+//     }
+// }
 
 #[proc_macro]
 pub fn hydroflow_parser(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = parse_macro_input!(input as Pipeline);
+    let input = parse_macro_input!(input as Expr);
     // input.into_token_stream().into()
     quote! { println!("hello world"); }.into()
 }
