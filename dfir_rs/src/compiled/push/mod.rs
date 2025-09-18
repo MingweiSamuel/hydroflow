3eb3b7129bfe59a40006f31e0de1162e5cf34b1c
Mingwei Samuel <mingwes@amazon.com>
2025-09-18 14:12:31 -0700

    partition(), demux_enum() working

--- dfir_rs/src/compiled/push/mod.rs
+++ dfir_rs/src/compiled/push/mod.rs
@@ -1,6 +1,5 @@
 //! Push-based operator helpers, i.e. [`futures::sink::Sink`] helpers.
 
-mod demux_enum;
 mod filter;
 mod filter_map;
 mod flat_map;
@@ -8,10 +7,10 @@ mod flatten;
 mod for_each;
 mod inspect;
 mod map;
+mod partition;
 mod persist;
 mod resolve_futures;
 mod unzip;
-pub use demux_enum::DemuxEnum;
 pub use filter::Filter;
 pub use filter_map::FilterMap;
 pub use flat_map::FlatMap;
@@ -19,6 +18,14 @@ pub use flatten::Flatten;
 pub use for_each::ForEach;
 pub use inspect::Inspect;
 pub use map::Map;
+pub use partition::{Partition, SinkVariadic};
 pub use persist::Persist;
 pub use resolve_futures::ResolveFutures;
 pub use unzip::Unzip;
+
+#[cfg(feature = "dfir_macro")]
+#[cfg_attr(docsrs, doc(cfg(feature = "dfir_macro")))]
+mod demux_enum;
+#[cfg(feature = "dfir_macro")]
+#[cfg_attr(docsrs, doc(cfg(feature = "dfir_macro")))]
+pub use demux_enum::DemuxEnum;
